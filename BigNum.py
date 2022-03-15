058793b0413cb9c528ba23a20c26217ed24e3ae6
Seyun Ahn <tpdbs0907@hanyang.ac.kr>
2022-03-15 23:43:04 +0900

    .

--- BigNum.py
+++ BigNum.py
@@ -5,21 +5,18 @@ num_list = list(map(int, input().split()))
 
 # 정렬
 num_list.sort() 
-print(num_list)
 
 # 첫 번째로 큰 수, 두 번째로 큰 수
 f_num = num_list[n-1]
 s_num = num_list[n-2]
-print(s_num)
 
 for j in range(m):
     if(cnt > 0):
         result += f_num
         cnt -= 1
-        print(result)
+
     else:
         result += s_num
         cnt = k
-        print(result)
 
 print(result)
