d96987a6fb08428a02993927ce1db822f69cc79a
Seyun Ahn <tpdbs0907@hanyang.ac.kr>
2022-03-16 07:21:43 +0900

    resolved

--- NumCardGame.py
+++ NumCardGame.py
@@ -1,11 +1,11 @@
 n, m = map(int, input().split())
 result = 0
 
-card = [list(map(int, input().split())) for _ in range(n)]
-min_num = min(card)
+for i in range(n):
+    card = list(map(int, input().split()))
+    min_num = min(card)
+    
+    if(min_num > result):
+        result = min_num
 
-if(min_num > result):
-    result = min_num
-else:
-    result = result
 print(result)
