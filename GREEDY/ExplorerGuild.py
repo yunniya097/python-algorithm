2a116c0a2430ab9ee46acf91917f6e6ac20055dc
yunniya097 <dbdhr0907@gmail.com>
2022-05-13 14:57:28 +0900

    solve

---
+++ GREEDY/ExplorerGuild.py
@@ -0,0 +1,18 @@
+n = int(input())
+explorer = list(map(int, input().split()))
+
+result = []
+cnt = 0
+
+explorer.sort() # 모험가 정렬
+
+while True:
+    m = max(explorer) # 모험가의 공포도 중 최댓값을 찾아서 m 에 저장
+    for i in range(m): # 최댓값 만큼 pop
+        result.append(explorer.pop()) 
+    cnt += 1
+
+    if len(explorer) == 0:
+        break
+
+print(cnt)
---
+++ IMPLEMENTATION/CandyGame.py
@@ -0,0 +1,10 @@
+n = int(input())
+candy = []
+
+def find_candy(a):
+    return 
+for i in range(n):
+    candy.append(list(input().split()))
+
+candy = sum(candy, [])
+print(candy)
